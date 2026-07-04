8079676465e974f6741fbbdf5fda0e4059055218
dennie <denitsa.sureva@icloud.com>
2026-07-04 14:12:35 +0200

    db config

--- lib/db.ts
+++ lib/db.ts
@@ -19,6 +19,42 @@ declare global {
 
 let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
 
-if (!global.mongoose) [
-    global.mongoose = cached
-]
+if (!global.mongoose) {
+    global.mongoose = cached;
+}
+
+
+// Copy from Doc
+
+async function connectDB() {
+  if (!MONGODB_URI) {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env"
+    );
+  }
+
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default connectDB;
