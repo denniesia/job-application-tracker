94898112256d79e7440b51b3053e0ba662d62999
dennie <denitsa.sureva@icloud.com>
2026-07-22 17:54:54 +0200

    caching board

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,28 +3,37 @@ import { getSession } from '../../lib/auth/auth';
 import connectDB from '../../lib/db';
 import { Board } from '../../lib/models';
 import KanbanBoard from '../../components/KanbanBoard';
+import { Suspense } from 'react';
 
-export default async function Dashboard() {
+async function getBoard(userId) {
+    'use cache';
+
+    await connectDB();
+
+    const boardDoc = await Board.findOne({
+        userId: userId,
+        name: 'Job Hunt',
+    }).populate({
+        path: 'columns',
+        populate: {
+            path: 'jobApplications',
+        },
+    });
+
+    if (!boardDoc) return null;
+
+    const board = JSON.parse(JSON.stringify(boardDoc));
+    
+    return board;
+}
+
+async function DashboardPage() {
     const session = await getSession();
+    const board = await getBoard(session?.user.id ?? '');
 
     if (!session?.user) {
         redirect('/sign-in');
     }
-
-  
-    await connectDB();
-
-    const board = await Board.findOne({
-        userId: session.user.id,
-        name: 'Job Hunt',
-    }).populate({
-        path: "columns",
-        populate: {
-            path: "jobApplications",
-        }
-    })
-    console.log(board);
-
 
     return (
         <div className="min-h-screen bg-white">
@@ -33,8 +42,16 @@ export default async function Dashboard() {
                     <h1 className="text-3xl font-bold text-black">Job Hunt</h1>
                     <p className="text-gray-600">Track your job applications</p>
                 </div>
-                <KanbanBoard board={JSON.parse(JSON.stringify(board))} userId={session.user.id} />
+                <KanbanBoard board={board} userId={session.user.id} />
             </div>
         </div>
+    );
+}
+
+export default async function Dashboard() {
+    return (
+        <Suspense fallback={<p>Loading...</p>}>
+            <DashboardPage />
+        </Suspense>
     );
 }
